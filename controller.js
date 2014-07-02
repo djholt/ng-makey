4706cc115333faec2f94e269975343f4c77a3f5b
CodeSchool <codeschool@scandium-10.local>
2014-07-02 11:34:47 -0600

    Make Flappy Bird

--- controller.js
+++ controller.js
@@ -1,43 +1,51 @@
 (function () {
   angular.module("makey")
     .controller("GameController", ["$scope", "$timeout", function ($scope, $timeout) {
-      $scope.boxPositionLeft = "20px";
-      $scope.boxPositionTop = "20px";
+      $scope.birdPositionLeft = 100;
+      $scope.birdPositionTop = 400;
 
-      $scope.keyPressed = function ($event) {
-        switch ($event.which) {
-          case 37: // left
-          case 65: // a
-            moveBox(-5, 0);
-            break;
-          case 38: // up
-          case 87: // w
-            moveBox(0, -5);
-            break;
-          case 39: // right
-          case 68: // d
-            moveBox(5, 0);
-            break;
-          case 40: // down
-          case 83: // s
-            moveBox(0, 5);
-            break;
-        }
+      $scope.boxes = [];
+      for (var i = 0; i < 100; i++) {
+        var r = Math.floor((Math.random() - 0.5) * 200);
+
+        var box1 = {};
+        box1.x = 200 + i * 150;
+        box1.y = -650 + r;
+        $scope.boxes.push(box1);
+
+        var box2 = {};
+        box2.x = 200 + i * 150;
+        box2.y = 500 + r;
+        $scope.boxes.push(box2);
+      }
+
+      var moveBoxes = function () {
+        angular.forEach($scope.boxes, function (box) {
+          box.x -= 2;
+        });
       };
 
-      var moveBox = function (dx, dy) {
-        console.log("moving");
-        var newX = parseInt($scope.boxPositionLeft, 10) + dx;
-        var newY = parseInt($scope.boxPositionTop, 10) + dy;
-        $scope.boxPositionLeft = "" + newX + "px";
-        $scope.boxPositionTop = "" + newY + "px";
+      var moveBird = function (dx, dy) {
+        $scope.birdPositionLeft += dx;
+        $scope.birdPositionTop += dy;
       };
 
       var gameLoop = function () {
-        moveBox(1, 0);
-        $timeout(gameLoop, 100);
+        moveBird(0, 3);
+        moveBoxes();
+        $timeout(gameLoop, 20);
       };
 
       gameLoop();
+
+      $scope.keyPressed = function ($event) {
+        switch ($event.which) {
+          case 32: // space
+          case 38: // up
+          case 87: // w
+            moveBird(0, -40);
+            break;
+        }
+      };
   }]);
 })();
